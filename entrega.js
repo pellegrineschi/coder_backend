5780ae23ba4beb2d3a3e81aac0e64b9826b4cd82
pellegrineschi <pellegrineschi86@gmail.com>
2023-12-24 11:49:14 -0300

    practicas

--- entrega.js
+++ entrega.js
@@ -5,45 +5,56 @@ class ProductManager {
   }
   //metodos
   // metodo para mostrar los productos
-  mostarProdutos() {
+  mostarVehiculos() {
     return this.vehiculos; // apunto hacia la propiedad produscto
   }
 
   // metodo para agregar los productos
   // el metodo con el parametro, en este caso el producto
-  agregarProducto(vheiculo) {
+  agregarVehiculo(vehiculos) {
     // validacion
     if (
-      !vheiculo.dominio ||
-      !vheiculo.marca ||
-      !vheiculo.modelo ||
-      !vheiculo.anio ||
-      !vheiculo.tipoCombustible
+      !vehiculos.dominio ||
+      !vehiculos.marca ||
+      !vehiculos.modelo ||
+      !vehiculos.anio ||
+      !vehiculos.tipoCombustible
     ) {
       console.log("todos los campos son nesesarios");
     }
-    if (//recorro el array buscado si se culple la condicion que el codigo ingresado sea igual a otro que ya alla en el array de producto
+    if (
+      //recorro el array buscado si se culple la condicion que el codigo ingresado sea igual a otro que ya alla en el array de producto
       this.vehiculos.some(
-        (vehiculoExist) => vehiculoExist.dominio === this.vehiculos.dominio
+        (vehiculoExist) => vehiculoExist.dominio === vehiculos.dominio
       )
     ) {
-      console.log("el vehiculo ya existe");//en caso que sea verdadero 
+      console.log("el vehiculo ya existe"); //en caso que sea verdadero
     }
-    vheiculo.id = this.cont++;//genero el id de manera autoincremental
-    this.vehiculos.push(this.vehiculos); // apunto hacia el array producto con el metodo push, para agregar el producto que me viene por parametro  desde el metodo
-
+    vehiculos.id = this.cont++; //genero el id de manera autoincremental
+    this.vehiculos.push(vehiculos); // apunto hacia el array producto con el metodo push, para agregar el producto que me viene por parametro  desde el metodo
   }
 
   // metodo buscar por id
-  bucarId(id){
-    const producto = this.producto.find(producto => producto.id === id)//recorro el array comparando el id q ingresa con los id dentro del array
-    if(producto){// en cado de encontarlo me devuelve el objeto
-        return producto;
-    }else{
-        console.log("el id no se encuentra");
-        return null;
+  bucarId(id) {
+    const vehiculo = this.vehiculos.find((vehiculo) => vehiculo.id === id); //recorro el array comparando el id q ingresa con los id dentro del array
+    if (vehiculo) {
+      // en cado de encontarlo me devuelve el objeto
+      return vehiculo;
+    } else {
+      console.log("el id no se encuentra");
+      return null;
     }
+  }
 
+  //metodo buscar por numero de dominio
+  buscarDominio(dominio){
+    const vehiculo = this.vehiculos.find((vehiculo) => vehiculo.dominio === dominio);
+    if (vehiculo){
+      return vehiculo;
+    }else{
+      console.log("el dominio no se encuentra");
+      return null;
+    }
   }
 }
 
@@ -53,20 +64,24 @@ const productManager = new ProductManager();
 
 //agrego productos
 
-productManager.agregarProducto({
-  titulo: "marolio",
-  descripcion: "arroz",
-  precio: 1000,
-  codigo:"ab111",
-  stock:100
+productManager.agregarVehiculo({
+  dominio: "fai281",
+  marca: "volkswagen",
+  modelo: "gol",
+  anio: 2005,
+  tipoCombustible: "nafta",
+});
 
-})
+productManager.agregarVehiculo({
+  dominio: "gqn495",
+  marca: "fiat",
+  modelo: "uno",
+  anio: 2007,
+  tipoCombustible: "diesel",
+});
 
-productManager.agregarProducto({
-  titulo:"marolio",
-  descripcion:"fideos",
-  precio:1200,
-  stock:120
-})
+// console.log(productManager.mostarVehiculos());
 
-console.log(productManager.mostarProdutos());
+// console.log(productManager.bucarId(3));
+
+console.log(productManager.buscarDominio("fai291"));
