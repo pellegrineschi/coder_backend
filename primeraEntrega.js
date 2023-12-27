a1f7851bef20929dd42623eca65e33d38e51c6e0
pellegrineschi <pellegrineschi86@gmail.com>
2023-12-27 12:41:23 -0300

    practica
    nodejs file system

--- primeraEntrega.js
+++ primeraEntrega.js
@@ -1,3 +1,7 @@
+const { json } = require('node:stream/consumers');
+
+const fs = require('fs').promises;
+
 // creacion de la clase
 
 class ProductManager {
@@ -5,10 +9,35 @@ class ProductManager {
   constructor() {
     this.products = [];
     this.nextId = 1;
+    this.filePath = 'products.json';
   }
 
+  //metodo para cargar los productos desde archivo
+  async loadProductFromFile(){
+    try{
+      const data = await fs.readFile(this.filePath,'utf-8');
+      this.products = JSON.parse(data); //pasea la cadena en un array
+    }
+    catch(err) {
+      console.error('Erro en la carga de productos desde archivo: ', err)
+    }
+  }
+
+  //metodo para guardar productos en archivo
+  async saveProductFile(){
+    try{
+      const data = JSON.stringify(this.products, null, 2); //convierte el array en una cadena
+      await fs.writeFile(this.filePath,data,'utf-8');
+      console.log('Productos guardados en el archivo: ', this.filePath)
+    }
+    catch(err) {
+      console.error('Error al guardar productos en archivo: ', err);
+    }
+  }
+
+
   //metodo agregar
-  addProduct(products) {
+  async addProduct(products) {
     //validamos
     if (
       !products.title ||
@@ -32,6 +61,7 @@ class ProductManager {
 
     products.id = this.nextId++;
     this.products.push(products);
+    await this.saveProductFile();
     console.log(`producto "${products.title}" agegado con ID ${products.id} `);
   }
 
@@ -47,8 +77,15 @@ class ProductManager {
   }
 
   //metodo obtener los productos
-  getProduts() {
+  async getProduts() {
+    await this.loadProductFromFile();
     return this.products;
+  }
+
+  // metodo para mostrar produsctos
+  async showProducts(){
+    const products = await productManager.getProduts();
+    console.log('Lista de productos: ', products)
   }
 }
 
@@ -75,20 +112,21 @@ productManager.addProduct({
 });
 
 // Intentar agregar un producto con código duplicado
-productManager.addProduct({
-  title: "Producto 3",
-  description: "Descripción del Producto 3",
-  price: 25,
-  thumbnail: "/images/product3.jpg",
-  code: "P001", // Este código ya existe
-  stock: 12,
-});
+// productManager.addProduct({
+//   title: "Producto 3",
+//   description: "Descripción del Producto 3",
+//   price: 25,
+//   thumbnail: "/images/product3.jpg",
+//   code: "P001", // Este código ya existe
+//   stock: 12,
+// });
 
 //traer los productos
-console.log(productManager.getProduts());
+// console.log(productManager.getProduts());
+productManager.showProducts();
 
 //buscar por id
-console.log(productManager.getProductById(1));
+// console.log(productManager.getProductById(1));
 
 //buscar por id que no esta
-console.log(productManager.getProductById(3));
+// console.log(productManager.getProductById(3));
